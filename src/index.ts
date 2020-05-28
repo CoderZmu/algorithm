cbf92a52b909e2b59471be83a67ffa038ae7af6e
zimu <muz64034@gmail.com>
2020-05-28 19:05:26 +0800

    3

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { generateRandomArray, testSort } from './sortTestHelper'
 
 import { quickSort, quickSort3Ways } from './sort/quickSort'
 
-let n = 1000
-let arr = generateRandomArray(n, 0, 10)
+let n = 1000000
+let arr = generateRandomArray(n, 0, n)
+let arr2 = [...arr]
+
+testSort('quickSort', quickSort, arr, n)
 testSort('quickSort3Ways', quickSort3Ways, arr, n)
